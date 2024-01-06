b70407da353ad22423ecb2c2921edc8232e34fec
Karanvir1729 <86384415+Karanvir1729@users.noreply.github.com>
2024-01-06 16:26:14 -0500

    review personality genertion

---
+++ Review Generators/OpenAI_review_generators.py
@@ -0,0 +1,36 @@
+#pip install openai
+import openai
+
+if __name__ == '__main__':
+    history = ""
+    openai.api_key = "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"
+    p = input("Hi! Hru?")
+    response = openai.Completion.create(
+        model="text-davinci-003",
+        prompt=p,
+        temperature=0,
+        max_tokens=150,
+        top_p=1.0,
+        frequency_penalty=1,
+        presence_penalty=1
+    )
+    ai_response = response["choices"][0]["text"]
+    print(ai_response)
+    history += f"me: {p}\n"
+    history += f"friend: {ai_response}\n"
+    while p != "bye":
+        p = input()
+        history += f"me: {p}\n"
+        history += "friend:"
+        response = openai.Completion.create(
+            model="text-davinci-003",
+            prompt=history,
+            temperature=0,
+            max_tokens=150,
+            top_p=1.0,
+            frequency_penalty=1,
+            presence_penalty=1
+        )
+        ai_response = response["choices"][0]["text"]
+        print(ai_response)
+        history += f" {ai_response}\n"
---
+++ Review Generators/review_personality_generator.py
@@ -0,0 +1,27 @@
+# Takes input from the front end about the creteria
+# Goal is to make chat gpt create personalities that are highly biased towards the key words.
+# pip install openai
+import openai
+
+personality_prompts = {}
+key_words = ["novelty", "nature", "climate change"]
+
+if __name__ == '__main__':
+
+    openai.api_key = "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"
+
+    for key in key_words:
+
+        p = f"Given a topic write a description of the person who only cares about this topic and nothing else. Topic: {key}"
+        response = openai.Completion.create(
+            model="text-davinci-003",
+            prompt=p,
+            temperature=0,
+            max_tokens=150,
+            top_p=1.0,
+            frequency_penalty=1,
+            presence_penalty=1
+        )
+        ai_response = response["choices"][0]["text"]
+        personality_prompts[key] = ai_response
+
